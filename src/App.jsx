64196c629503ea8095804d5b0f011c10e18d0b0f
sabincoding01 <sabintimalsina74@gmail.com>
2026-02-12 10:56:04 +0545

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from "./components/Navbar/NavBar";
 
-const App = () => {
+import Dashboard from "./pages/Dashboard";
+import WaterIntake from "./pages/WaterIntake";
+import Exercise from "./pages/Exercise";
+import Tips from "./pages/Tips";
+
+function App() {
   return (
-    <div>
-      <h1 className="font-bold">sxabin</h1>
-    </div>
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/water" element={<WaterIntake />} />
+        <Route path="/exercise" element={<Exercise />} />
+        <Route path="/tips" element={<Tips />} />
+      </Routes>
+    </BrowserRouter>
   );
-};
+}
 
 export default App;
--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  colors: { ecoGray: "#F0EFEE" }, // custom name for your color },
   plugins: [tailwindcss()],
 });
